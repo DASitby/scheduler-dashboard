34388b72e6160a3f21ad8f66a3c6807237344475
DASitby <davidsooley551@gmail.com>
2023-04-24 14:23:36 -0230

    create panel component and import to dashboard, render with props from dummy data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Loading from "./Loading";
+import Panel from "./Panel";
 import classnames from "classnames";
 
 const data = [
@@ -33,12 +34,19 @@ class Dashboard extends Component {
 
   render() {
     const dashboardClasses = classnames("dashboard");
-
     if (this.state.loading) {
       return <Loading />;
     }
+    const panels = data.map(element => {
+      return <Panel
+      key={element.id}
+      id={element.id}
+      label={element.label}
+      value={element.value}
+      />
+    });
 
-    return <main className={dashboardClasses} />;
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
